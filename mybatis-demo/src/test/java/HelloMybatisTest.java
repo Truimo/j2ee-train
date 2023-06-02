159b9a6d16a2f39a040de0924be642d63bc3f801
Truimo <xiaomo@20mo.cn>
2023-06-02 16:44:35 +0800

    feat: mybatis crud

--- mybatis-demo/src/test/java/HelloMybatisTest.java
+++ mybatis-demo/src/test/java/HelloMybatisTest.java
@@ -8,6 +8,10 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HelloMybatisTest {
     @Test
@@ -18,5 +22,90 @@ public class HelloMybatisTest {
         UserDao userDao = sqlSession.getMapper(UserDao.class);
         User user = userDao.selectUserById(1);
         System.out.println(user);
+    }
+
+    @Test
+    public void test02() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        User user = userDao.selectUserByIdAndPwd(1, "123456");
+        System.out.println(user);
+    }
+
+    @Test
+    public void test03() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("id", 1);
+        map.put("password", "123456");
+        User user = userDao.selectUserByMap(map);
+        System.out.println(user);
+    }
+
+    @Test
+    public void test04() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        User user1 = new User();
+        user1.setId(1);
+        user1.setPassword("123456");
+        User user = userDao.selectUserByObject(user1);
+        System.out.println(user);
+    }
+
+    @Test
+    public void test05() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        List<User> users = userDao.selectUserByKeyWords("李");
+        System.out.println(users);
+    }
+
+    @Test
+    public void test06() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        int i = userDao.deleteUserById(1);
+        sqlSession.commit();
+        System.out.println(i);
+    }
+
+    @Test
+    public void test07() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        User user = new User();
+        user.setId(1);
+        user.setName("小明");
+        int i = userDao.updateUserById(user);
+        sqlSession.commit();
+        System.out.println(i);
+    }
+
+    @Test
+    public void test08() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        ArrayList<Integer> ids = new ArrayList<>();
+        ids.add(1);
+        ids.add(2);
+        int i = userDao.deleteUsersByIds(ids);
+        sqlSession.commit();
+        System.out.println(i);
     }
 }
