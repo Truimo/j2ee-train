2bedf8888c571b2114f023b558fe5b0c9292bc45
Truimo <xiaomo@20mo.cn>
2023-06-03 09:02:28 +0800

    feat: created UserDaoImpl

---
+++ mybatis-demo/src/main/java/org/example/dao/impl/UserDaoImpl.java
@@ -0,0 +1,11 @@
+package org.example.dao.impl;
+
+import org.example.dao.UserDao;
+import org.example.entity.User;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User selectUserById(Integer id) {
+        return null;
+    }
+}
