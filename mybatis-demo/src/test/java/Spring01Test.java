6d40ff619f00017fba6ed7e63451b5cf10bb6970
Truimo <xiaomo@20mo.cn>
2023-06-03 15:46:10 +0800

    test: User, Student beans test

--- mybatis-demo/src/test/java/Spring01Test.java
+++ mybatis-demo/src/test/java/Spring01Test.java
@@ -1,4 +1,6 @@
 import org.example.dao.UserDao;
+import org.example.entity.Student;
+import org.example.entity.User;
 import org.example.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -11,5 +13,19 @@ public class Spring01Test {
         UserDao userDao = (UserDao) context.getBean("userDao");
         UserService userService = (UserService) context.getBean("userService");
         userService.selectUserById(1);
+    }
+
+    @Test
+    public void test02() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        User user = (User) context.getBean("ul");
+        System.out.println(user);
+    }
+
+    @Test
+    public void test03() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        Student stu = (Student) context.getBean("stu");
+        System.out.println(stu);
     }
 }
