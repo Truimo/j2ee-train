560bf07335a021f350bfc91719bfc8b23b331234
Truimo <xiaomo@20mo.cn>
2023-06-03 09:01:21 +0800

    feat: UserDao keep only selectUserByIdAndPwd

--- mybatis-demo/src/main/java/org/example/dao/UserDao.java
+++ mybatis-demo/src/main/java/org/example/dao/UserDao.java
@@ -1,26 +1,8 @@
 package org.example.dao;
 
-import org.apache.ibatis.annotations.Param;
 import org.example.entity.User;
-
-import java.util.List;
-import java.util.Map;
 
 public interface UserDao {
     public User selectUserById(Integer id);
 
-    //    public User selectUserByIdAndPwd(Integer id, String password);
-    public User selectUserByIdAndPwd(@Param("id") Integer id, @Param("password") String password);
-
-    public User selectUserByMap(Map<Object, Object> values);
-
-    public User selectUserByObject(User user);
-
-    public List<User> selectUserByKeyWords(String keyWords);
-
-    public int deleteUserById(@Param("id") Integer id);
-
-    public int updateUserById(User user);
-
-    public int deleteUsersByIds(@Param("ids") List<Integer> ids);
 }
